1f87d96f1e21aeb5316d5b2bbb09bc8ad9d6e6c7
Vedikakoravi <159905384+Vedikakoravi@users.noreply.github.com>
2024-08-07 20:39:58 +0530

    Create Pyramid.java
    Program to make such a pattern like a right angle triangle with the number increased by  1.

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Pyramid
+{
+public static void main(String args[])
+{
+int i,j,n,k=1;
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the number ");
+n=sc.nextInt();
+
+for(i=1;i<n;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(k + " ");
+k++;
+}
+System.out.println("");
+}
+}
+}
